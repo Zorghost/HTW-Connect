77597e7fc066e90c8130190ea33ede2a15697be7
zorghost <Rayen.Bedoui@Student.HTW-Berlin.de>
2022-04-08 21:03:33 +0200

    enabling the to show another window from the login with a button click event

---
+++ HTW-Connect/login.h
@@ -0,0 +1,25 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+#include "chat.h"
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Login; }
+QT_END_NAMESPACE
+
+class Login : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Login(QWidget *parent = nullptr);
+    ~Login();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::Login *ui;
+    chat *chat;
+};
+#endif // LOGIN_H

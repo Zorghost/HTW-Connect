880225da046ab93498389408096a8498a57d1c2d
zorghost <Rayen.Bedoui@Student.HTW-Berlin.de>
2022-04-10 22:34:28 +0200

    modifying login forms

--- build-HTW-Connect-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/ui_login.h
+++ build-HTW-Connect-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/ui_login.h
@@ -11,6 +11,8 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
@@ -24,6 +26,10 @@ class Ui_Login
 public:
     QWidget *centralwidget;
     QPushButton *pushButton;
+    QLabel *label;
+    QLabel *label_2;
+    QLineEdit *lineEdit;
+    QLineEdit *lineEdit_2;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -36,7 +42,20 @@ public:
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         pushButton = new QPushButton(centralwidget);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(290, 260, 75, 24));
+        pushButton->setGeometry(QRect(330, 330, 75, 24));
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(280, 160, 71, 16));
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(280, 240, 49, 16));
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+        lineEdit->setGeometry(QRect(280, 180, 113, 22));
+        lineEdit_2 = new QLineEdit(centralwidget);
+        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+        lineEdit_2->setGeometry(QRect(280, 260, 113, 22));
+        lineEdit_2->setInputMethodHints(Qt::ImhHiddenText|Qt::ImhSensitiveData);
         Login->setCentralWidget(centralwidget);
         menubar = new QMenuBar(Login);
         menubar->setObjectName(QString::fromUtf8("menubar"));
@@ -54,7 +73,9 @@ public:
     void retranslateUi(QMainWindow *Login)
     {
         Login->setWindowTitle(QCoreApplication::translate("Login", "Login", nullptr));
-        pushButton->setText(QCoreApplication::translate("Login", "PushButton", nullptr));
+        pushButton->setText(QCoreApplication::translate("Login", "Login", nullptr));
+        label->setText(QCoreApplication::translate("Login", "username", nullptr));
+        label_2->setText(QCoreApplication::translate("Login", "Password", nullptr));
     } // retranslateUi
 
 };
